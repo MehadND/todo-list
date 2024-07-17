e87bfaff668cb415822453c5b017776f678dbabe
Fahad tufail <tufailfahad01@gmail.com>
2024-07-18 00:10:27 +0500

    Add assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
 'use client';
+import { useEffect } from 'react';
+import Image from 'next/image';
+
+import { Box } from '@mui/material';
+import { toast } from 'react-toastify';
+
 import { useAppDispatch, useAppSelector } from '@/lib/redux';
 import { addTask, addTaskList } from '@/lib/redux/reducers/todoList.reducers';
-import { useEffect } from 'react';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.min.css';
+import icons from './_assets/svgs';
 
 export default function Home() {
   const { taskLists } = useAppSelector((state) => state.taskListCollection);
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    toast.success('hello');
-  }, []);
-  return <h1>hello</h1>;
+  useEffect(() => {}, []);
+
+  return (
+    <Box>
+      <Image src={icons.addIcon} alt="addIcon" />
+    </Box>
+  );
 }
